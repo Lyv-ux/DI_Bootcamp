c964fb17d3f5e466856bd81dc8b3483e04f59b50
Lyv-ux <oliviague07@gmail.com>
2026-05-21 08:47:31 +0000

    Renaming files and folders

---
+++ Week01/Day02/ExercisesXP_Gold.py
@@ -0,0 +1,5 @@
+#Exercise 1: Birthday Look-up
+#Create a variable called birthdays. Its value should be a dictionary and Initialize this variable with the birthdays of 5 people of your choice. For each entry in the dictionary, the key should be the person’s name, and the value should be their birthday. Tip: Use the format "YYYY/MM/DD".
+
+birthdays = {}
+#
