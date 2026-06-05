fc34e248567997ecd92d208f720e0764a715ed63
Lyv-ux <oliviague07@gmail.com>
2026-06-05 05:23:57 +0000

    First Commit

---
+++ week03/test_hackathon.py
@@ -0,0 +1,16 @@
+Class User():
+    def __init__(self, name, age, password):
+       
+
+
+Class Cat():
+     def __init__(self, age, name):
+         self.age = age
+         self.name = name       
+
+
+class Farm:
+    # Étape 2 : On prépare le nom et le dictionnaire vide
+    def __init__(self, farm_name):
+        self.name = farm_name
+        self.animals = {}  # Stockera par exemple : {'cow': 5, 'sheep': 2}
